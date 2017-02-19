688abb22d2bd31b8f989da44e5f615088a467474
kaideyi <kaideyi@zoneyet.com>
2017-02-19 15:02:18 +0800

    Init SlideTabBar

---
+++ SlideTabBar/SlideTabBar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SlideTabBar
+//
+//  Created by kaideyi on 2017/2/19.
+//  Copyright © 2017年 kaideyi.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
