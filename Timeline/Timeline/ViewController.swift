d9e84054e80bbc45d699b9a33409654e39f2dd7f
kaideyi <kaideyi@haodai.com>
2017-03-03 11:42:04 +0800

    Init Timeline Project

---
+++ Timeline/Timeline/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Timeline
+//
+//  Created by mac on 17/3/3.
+//  Copyright © 2017年 mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+     
+        self.title = "Timelines"
+    }
+}
+
