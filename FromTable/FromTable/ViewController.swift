8b7fe22a4234617f8afd3c1b5e726cabfa37d515
kaideyi <kaideyi@haodai.com>
2017-03-09 14:04:46 +0800

    Init FromTable Project

---
+++ FromTable/FromTable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FromTable
+//
+//  Created by mac on 17/3/9.
+//  Copyright © 2017年 mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+     
+        self.title = "Froms"
+        self.view.backgroundColor = .white
+    }
+}
+
