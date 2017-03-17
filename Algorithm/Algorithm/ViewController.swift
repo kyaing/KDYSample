a42aaa4a53fbf5518f7ce42fed4d2ebdce081687
kaideyi <kaideyi@zoneyet.com>
2017-03-17 17:10:00 +0800

    Init Algorithm Project

---
+++ Algorithm/Algorithm/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Algorithm
+//
+//  Created by kaideyi on 2017/3/17.
+//  Copyright © 2017年 kaideyi.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
