539481c3eda18ea4eee93fde32757bf6b8778b8f
kaideyi <kaideyi@haodai.com>
2017-03-29 15:58:28 +0800

    添加选择图片按钮

--- ImagePicker/ImagePicker/ViewController.swift
+++ ImagePicker/ImagePicker/ViewController.swift
@@ -13,8 +13,24 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.title = "ImagePikcer"
+        self.title = "ImagePicker"
         self.view.backgroundColor = .white
+        
+        let photoBtn = UIButton()
+        photoBtn.setTitle("选择图片", for: .normal)
+        photoBtn.setTitleColor(.red, for: .normal)
+        self.view.addSubview(photoBtn)
+        
+        photoBtn.addTarget(self, action: #selector(ViewController.selectPhotos), for: .touchUpInside)
+        
+        photoBtn.snp.makeConstraints { (make) in
+            make.center.equalTo(self.view)
+            make.size.equalTo(CGSize(width: 150, height: 40))
+        }
+    }
+    
+    func selectPhotos() {
+        print("selectPhotos")
     }
 }
 
