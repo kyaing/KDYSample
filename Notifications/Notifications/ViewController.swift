1d2ec5a2cb3805ca8d9b7e2e6ffd2decfafb83bf
kaideyi <kaideyi@haodai.com>
2017-04-05 10:41:30 +0800

    Init Notifications Project

---
+++ Notifications/Notifications/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Notifications
+//
+//  Created by mac on 17/4/5.
+//  Copyright © 2017年 mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.title = "Notifications"
+        self.view.backgroundColor = .white
+    }
+}
+
