25f774e1b9023e3b8f10ba2e5bb982b523b79fd7
kaideyi <kaideyi@haodai.com>
2017-04-11 17:40:36 +0800

    Init KYAudio Project

---
+++ KYAudio/KYAudio/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KYAudio
+//
+//  Created by mac on 17/4/11.
+//  Copyright © 2017年 mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.title = "音频"
+        self.view.backgroundColor = .white
+    }
+}
+
