a7ac65a1d40d2bfb1f7467c37d7bc42f394ce898
kaideyi <kaideyi@zoneyet.com>
2017-05-14 16:06:48 +0800

    Update KyDownload (开始学习尝试封装下载模块)

--- KYPlayer/KYPlayer/KYPlayer/KYDownload/KYDownloadManager.swift
+++ KYPlayer/KYPlayer/KYPlayer/KYDownload/KYDownloadManager.swift
@@ -8,8 +8,21 @@
 
 import UIKit
 
-/// 视频下载管理
+/**
+ * 文件下载管理类
+ * (支持的主要功能：文件存取、后台下载、断点续传等)
+ */
+
 class KYDownloadManager: NSObject {
     
+    // MARK: - Properties
+    
+    static let `default` = KYDownloadManager()
+    
+    // MARK: - Life Cycle
+    
+    fileprivate override init() {
+        super.init()
+    }
 }
 
---
+++ KYPlayer/KYPlayer/KYPlayer/KYDownload/KYDownloadTool.swift
@@ -0,0 +1,19 @@
+//
+//  KYDownloadTool.swift
+//  KYPlayer
+//
+//  Created by kaideyi on 2017/5/13.
+//  Copyright © 2017年 mac. All rights reserved.
+//
+
+import UIKit
+
+class KYDownloadTool: NSObject {
+
+    // MARK: - Public Methods
+    
+    func getFileName() -> String? {
+        return nil
+    }
+}
+
