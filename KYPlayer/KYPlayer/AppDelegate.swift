f9a73dd5ccd03033a4c67e1b4a3e7b249b73b66c
kaideyi <kaideyi@haodai.com>
2017-05-19 11:37:53 +0800

    使用DispatchGroup解决异步相互依赖

--- KYPlayer/KYPlayer/AppDelegate.swift
+++ KYPlayer/KYPlayer/AppDelegate.swift
@@ -21,7 +21,34 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window?.rootViewController = KYPlayerViewController()
         window?.makeKeyAndVisible()
         
+        testGCDGroup()
+        
         return true
+    }
+    
+    func testGCDGroup() {
+        /** 
+         *  怎么解决多线程的异步依赖问题？这是一个经典的问题，
+         *  能最先想到的是GCD组，但怎么保证顺序问题，参考：http://www.jianshu.com/p/480d0c64d63c
+         */
+        
+        let group = DispatchGroup()
+        
+        group.enter()
+        DispatchQueue.global().async { 
+            print("线程一");
+            group.leave()
+        }
+        
+        group.enter()
+        DispatchQueue.global().async { 
+            print("线程二");
+            group.leave()
+        }
+        
+        group.notify(queue: DispatchQueue.main) { 
+            print("主线程");
+        }
     }
 }
 
