506ac8e2e4e173f35f5276931397390625f22cf1
kaideyi <kaideyi@haodai.com>
2017-08-10 15:06:52 +0800

    Init KYWebo Project

---
+++ KYWebo/KYWebo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KYWebo
+//
+//  Created by KYCoder on 2017/8/10.
+//  Copyright © 2017年 mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
