39b316251a5d6f228dd177f52820ff8219bb4ece
kyaing <kaideyi@haodai.com>
2017-12-18 14:34:19 +0800

    init KYForm

---
+++ KYForm/KYForm/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  KYForm
+//
+//  Created by mac on 2017/12/18.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KYForm/KYForm/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  KYForm
+//
+//  Created by mac on 2017/12/18.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
