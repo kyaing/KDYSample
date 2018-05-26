61a131b9351e701a4b98b30a8d99eaaf31ee0815
kyaing <kyaing@gmail.com>
2018-05-26 21:08:08 +0800

    # Django - myblog 使用模板与过滤器处理博客

--- kYPython/Django/myblog/blog/urls.py
+++ kYPython/Django/myblog/blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import blog_list, blog_detail
+from . import views
 
 urlpatterns = [
     # http://localhost:8000/blog/1
-    path('<int:blog_pk>', blog_detail, name='blog_detail'),
+    path('<int:blog_pk>', views.blog_detail, name='blog_detail'),
+    # http://localhost:8000/blog/type/1
+    path('type/<int:blog_type_pk>', views.blogs_with_type, name='blogs_with_type'),
 ]
