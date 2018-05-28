b7ea465e71263d14ae23b512c56bcbf41c57d84c
Mac <kaideyi@haodai.com>
2018-05-28 16:52:19 +0800

    # Django - myblog 使用CSS修饰首页

--- kYPython/Django/myblog/blog/urls.py
+++ kYPython/Django/myblog/blog/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # http://localhost:8000/blog/
+    path('', views.blog_list, name='blog_list'),
     # http://localhost:8000/blog/1
     path('<int:blog_pk>', views.blog_detail, name='blog_detail'),
     # http://localhost:8000/blog/type/1
