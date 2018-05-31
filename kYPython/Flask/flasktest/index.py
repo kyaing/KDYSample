7a5b78ea458d198dad52def6fa52bd5c8447c47f
Mac <kaideyi@haodai.com>
2018-05-31 16:30:26 +0800

    # Flask - flasktest

--- kYPython/Flask/flasktest/index.py
+++ kYPython/Flask/flasktest/index.py
@@ -1,18 +1,79 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+from flask_sqlalchemy import SQLAlchemy
+
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+# app.config字典存储框架、扩展和程序本身的配置变量
+app.config['SECRET_KEY'] = '@KY*))!!#@#@>/(#@-==￥**12'  
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+
 bootstrap = Bootstrap(app)
+db = SQLAlchemy(app)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
-
+    form = NameForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.name.data).first() 
+        if user is None:
+            user = User(username = form.name.data)
+            db.session.add(user)
+            session['known'] = False
+        else:
+            session['known'] = True
+        session['name'] = form.name.data
+        form.name.data = ''
+        return redirect(url_for('index'))
+    return render_template('index.html',
+            form = form, name = session.get('name'),
+            known = session.get('known', False))
 
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html')
+
+'''
+表单类
+'''
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
+
+'''
+Role Model
+'''
+class Role(db.Model):
+    __tablename__ = 'roles'   # 指定表名
+    id = db.Column(db.Integer, primary_key=True)   # 指定列名
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role') # 自行找到外键
+
+    def __repr__(self):
+        return '<Role: %r>' % self.name
+
+'''
+User Model
+'''
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 if __name__ == '__main__':
     app.run()
